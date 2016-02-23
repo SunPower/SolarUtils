28a9969829cc2b8203e5164e2a9ac26fcc6cdd0a
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2016-02-23 02:21:36 -0800

    remove darwin and linux

--- solar_utils/darwin/src/solposAM.h
+++
@@ -1,9 +0,0 @@
-// solposAM function definition
-//
-// 2013 Sunpower Corp.
-// Confidential & Proprietary
-// Do Not Distribute
-
-long solposAM( float *location, int *datetime, float *weather,
-    float *angles, float *airmass, int *settings, float *orientation,
-    float *shadowband );

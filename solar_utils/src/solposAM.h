582de82a1e2fb9bae4ca0a2d9345607a517d6ce5
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2016-02-22 22:37:52 -0800

    add macro to solpos.c

---
+++ solar_utils/src/solposAM.h
@@ -0,0 +1,16 @@
+// solposAM function definition
+//
+// 2013 Sunpower Corp.
+// Confidential & Proprietary
+// Do Not Distribute
+
+// define a macro for __declspec(dllexport) keyword instead of .def file
+#ifdef WIN32
+# define DllExport   __declspec( dllexport )
+#else
+# define DllExport
+#endif
+
+DllExport long solposAM( float *location, int *datetime, float *weather,
+    float *angles, float *airmass, int *settings, float *orientation,
+    float *shadowband );
